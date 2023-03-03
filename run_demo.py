5b37a7d970fbb716981d62c97d2309285cf25dcf
EmilStevnsborg <emilstevnsborg.official@gmail.com>
2023-03-03 10:59:24 +0100

    Path changes in demo

--- run_demo.py
+++ run_demo.py
@@ -8,6 +8,10 @@ import os
 from PIL import Image
 from pathlib import Path
 from tqdm import tqdm
+import os
+
+dir = os.getcwd()
+print(dir)
 
 
 def load_image(filename, size):
@@ -51,11 +55,11 @@ class Demo(nn.Module):
         self.args = args
 
         if args.model == 'vox':
-            model_path = 'checkpoints/vox.pt'
+            model_path = dir + '/checkpoints/vox.pt'
         elif args.model == 'taichi':
-            model_path = 'checkpoints/taichi.pt'
+            model_path = dir + '/checkpoints/taichi.pt'
         elif args.model == 'ted':
-            model_path = 'checkpoints/ted.pt'
+            model_path = dir + '/checkpoints/ted.pt'
         else:
             raise NotImplementedError
 
